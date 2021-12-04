db41772919b8539d73fff6c4afcc99dc51f17482
Rina <fghjiff8@gmail.com>
2021-12-04 18:36:28 +0900

    navbar진행중

--- ECOMMERCEAPI/client/src/components/Navbar.jsx
+++ ECOMMERCEAPI/client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Search } from "@material-ui/icons";
 
 const Container = styled.div`
   height: 60px;
@@ -7,12 +8,48 @@ const Container = styled.div`
 
 const Wrapper = styled.div`
   padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-content: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solide lightgrey;
+  display: flex;
+  align-items: center;
+`;
+
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
 `;
 
 const Navbar = () => {
   return (
     <Container>
-      <Wrapper>navbar</Wrapper>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            input
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
     </Container>
   );
 };
