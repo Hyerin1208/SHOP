b103b86c1485ad214f713d9d7c57570309ef7604
Rina <fghjiff8@gmail.com>
2021-12-05 11:04:22 +0900

    slider

--- ECOMMERCEAPI/client/src/components/Slider.jsx
+++ ECOMMERCEAPI/client/src/components/Slider.jsx
@@ -2,6 +2,7 @@ import {
   ArrowBackIosOutlined,
   ArrowForwardIosOutlined,
 } from "@material-ui/icons";
+import { useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -9,24 +10,31 @@ const Container = styled.div`
   height: 100vh;
   display: flex;
   background-color: coral;
+  position: relative;
 `;
 
 const Arrow = styled.div`
   width: 50px;
   height: 50px;
-  background-color: #fff7f7;
+  /* background-color: #fff7f7; */
   border-radius: 50%;
   display: flex;
   align-items: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
 `;
 
 const slider = () => {
   return (
     <Container>
-      <Arrow>
+      <Arrow direction="left">
         <ArrowBackIosOutlined />
       </Arrow>
-      <Arrow>
+      <Arrow direction="right">
         <ArrowForwardIosOutlined />
       </Arrow>
     </Container>
