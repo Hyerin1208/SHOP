e44373e199c9947b0dc0034d643047226e800af1
Rina <fghjiff8@gmail.com>
2021-12-05 10:54:22 +0900

    메인페이지 슬라이더

---
+++ ECOMMERCEAPI/client/src/components/Slider.jsx
@@ -0,0 +1,36 @@
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from "@material-ui/icons";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: coral;
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+`;
+
+const slider = () => {
+  return (
+    <Container>
+      <Arrow>
+        <ArrowBackIosOutlined />
+      </Arrow>
+      <Arrow>
+        <ArrowForwardIosOutlined />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default slider;
