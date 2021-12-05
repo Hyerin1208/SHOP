8bb3ca6f7c4173c71564017379607e747f8d71f8
Rina <fghjiff8@gmail.com>
2021-12-05 18:49:08 +0900

    메인스라이더

--- ECOMMERCEAPI/client/src/components/Slider.jsx
+++ ECOMMERCEAPI/client/src/components/Slider.jsx
@@ -2,14 +2,12 @@ import {
   ArrowBackIosOutlined,
   ArrowForwardIosOutlined,
 } from "@material-ui/icons";
-import { useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
 `;
 
@@ -26,6 +24,51 @@ const Arrow = styled.div`
   left: ${(props) => props.direction === "left" && "10px"};
   right: ${(props) => props.direction === "right" && "10px"};
   margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+  margin-left: 20%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 const slider = () => {
@@ -34,6 +77,21 @@ const slider = () => {
       <Arrow direction="left">
         <ArrowBackIosOutlined />
       </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src="//www.net-a-porter.com/variants/images/22250442025885711/fr/w2000_q60.jpg" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>블랙 프라이데이 세일</Title>
+            <Desc>
+              전 품목 10%세일, 12/6 - 12/9 pm11:59 자세한 내용은 꼭! 클릭하여
+              확인해 주세요
+            </Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
       <Arrow direction="right">
         <ArrowForwardIosOutlined />
       </Arrow>
