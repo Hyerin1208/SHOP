47da923018cc6bc8a0449f20e16a48929b3b2afc
Rina <fghjiff8@gmail.com>
2021-12-06 15:30:25 +0900

    제품리스트추가

--- ECOMMERCEAPI/client/src/data.js
+++ ECOMMERCEAPI/client/src/data.js
@@ -25,23 +25,52 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: "/image/pngwing.com (1).png",
-    title: "블랙 프라이데이 세일",
-    desc: "전 품목 10%세일, 12/6 - 12/9 자세한 내용은 꼭! 클릭하여 확인",
-    bg: "f5fafd",
+    img: "https://www.net-a-porter.com/content/images/cms/ycm/resource/blob/700462/fce766c57f3fd5a3aa25a40d9b7ee92f/incrediblejewels-mobilehptriopromo2-1200x900-data.jpg/w800_q80.jpg",
+    title: "LOULOU STUDIO",
   },
   {
     id: 2,
-    img: "/image/pngwing.com (2).png",
-    title: "788 NEW ITEMS New In",
-    desc: "Stay ahead of the style curve with our latest arrivals",
-    bg: "fcf1ed",
+    img: "https://www.net-a-porter.com/content/images/cms/ycm/resource/blob/700396/f47df2c29b263539e4c481f7606aa923/party-mobilehpduopromo1-1200x900-data.jpg/w800_q80.jpg",
+    title: "Dress the part(y)",
   },
   {
     id: 3,
-    img: "/image/pngwing.com.png",
-    title: "Wish-list-worthy gifts",
-    desc: "Delve into our gifting dreamland",
-    bg: "f5ebfc",
+    img: "https://www.net-a-porter.com/content/images/cms/ycm/resource/blob/701552/4297c20eecfe34613296d39333ff0c0a/mobile-single-promo-1200x900-data.jpg/w800_q80.jpg",
+    title: "Holiday styles",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "/image/pngwing.com (3).png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
   },
 ];
