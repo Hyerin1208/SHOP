096e3da50a2a2680dca645119f5fd05a68c177c4
Rina <fghjiff8@gmail.com>
2021-12-07 05:10:40 +0900

    footer home

--- ECOMMERCEAPI/client/src/components/Footer.jsx
+++ ECOMMERCEAPI/client/src/components/Footer.jsx
@@ -1,4 +1,12 @@
-import { Facebook, Instagram, Pinterest, Twitter } from "@material-ui/icons";
+import {
+  AddLocation,
+  EmailOutlined,
+  Facebook,
+  Instagram,
+  LocalPhone,
+  Pinterest,
+  Twitter,
+} from "@material-ui/icons";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -14,18 +22,55 @@ const Left = styled.div`
 
 const Logo = styled.h1``;
 
-const Desc = styled.p``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
 
-const SocialContainer = styled.div``;
+const SocialContainer = styled.div`
+  display: flex;
+`;
 
-const SocialIcon = styled.div``;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
 
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
 `;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
 const Right = styled.div`
   flex: 1;
 `;
+
+const ContactItem = styled.div``;
+
+const Payment = styled.img``;
 
 const Footer = () => {
   return (
@@ -38,22 +83,49 @@ const Footer = () => {
           attire
         </Desc>
         <SocialContainer>
-          <SocialIcon>
+          <SocialIcon color="3B5999">
             <Facebook />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E4405F">
             <Instagram />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="55ACEE">
             <Twitter />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E60023">
             <Pinterest />
           </SocialIcon>
         </SocialContainer>
       </Left>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fasion</ListItem>
+          <ListItem>Woman Fasion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <AddLocation />
+          622 Dixie Path, South Tobinchester 98336
+        </ContactItem>
+        <ContactItem>
+          <LocalPhone />+ 1 234 56 78
+        </ContactItem>
+        <ContactItem>
+          <EmailOutlined />
+          doredome@naver.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
     </Container>
   );
 };
