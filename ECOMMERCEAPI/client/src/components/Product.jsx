a2d700d59450d469f892870d40c3fb426a2d92df
Rina <fghjiff8@gmail.com>
2021-12-06 16:32:29 +0900

    product

--- ECOMMERCEAPI/client/src/components/Product.jsx
+++ ECOMMERCEAPI/client/src/components/Product.jsx
@@ -14,13 +14,34 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   background-color: #f5fbfd;
+  position: relative;
 `;
 
-const Circle = styled.div``;
+const Circle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+`;
+
 const Image = styled.img`
   height: 75%;
+  z-index: 2;
 `;
-const Info = styled.div``;
+
+const Info = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: gray;
+  z-index: 3;
+  display: flex;
+  ali
+`;
+
 const Icon = styled.div``;
 
 const Product = ({ item }) => {
