4bdae7b98294f0c50bd7465b7a1e2e5e8eb29b46
Rina <fghjiff8@gmail.com>
2021-12-07 20:53:56 +0900

    메인페이지 반응형OK

--- ECOMMERCEAPI/client/src/components/CategoryItem.jsx
+++ ECOMMERCEAPI/client/src/components/CategoryItem.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   flex: 1;
@@ -10,6 +11,7 @@ const Container = styled.div`
 const Image = styled.img`
   width: 100%;
   object-fit: cover;
+  ${mobile({ height: "60vh" })}
 `;
 
 const Info = styled.div`
