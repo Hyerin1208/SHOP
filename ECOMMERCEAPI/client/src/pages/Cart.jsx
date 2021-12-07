c28d14e5d8f015e4d4cc7f65230d335b3fe448f8
Rina <fghjiff8@gmail.com>
2021-12-07 16:19:21 +0900

    cart page

---
+++ ECOMMERCEAPI/client/src/pages/Cart.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import React from "react";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+
+const Container = styled.div``;
+
+const Cart = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      cart
+      <Footer />
+    </Container>
+  );
+};
+
+export default Cart;
