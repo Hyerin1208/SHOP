c26f03913eae1ace70224e4564a3d48cee5437df
Rina <fghjiff8@gmail.com>
2021-12-09 23:52:38 +0900

    reoutes 수정 , 대소문오타

--- ECOMMERCEAPI/api/routes/product.js
+++ ECOMMERCEAPI/api/routes/product.js
@@ -48,8 +48,8 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 // GET Product
 router.get("/find/:id", async (req, res) => {
   try {
-    const Product = await Product.findById(req.params.id);
-    res.status(200).json(Product);
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json(err);
   }
