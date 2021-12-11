df8895ed6999fe9f52af0c165c9c6dd03e0a7767
Rina <fghjiff8@gmail.com>
2021-12-11 16:00:24 +0900

    admin페이지와 node api fetching

--- ECOMMERCEAPI/client/src/requestMethods.js
+++ ECOMMERCEAPI/client/src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYThjNjNjY2FlOTQ4ZjY2NzljNmMyNCIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2MzkwMzcyMTEsImV4cCI6MTYzOTI5NjQxMX0.taj_u8w7mXJy4ToIaqNwitzYXAbi6TeMHwUerf8NZIg";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+  .currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
